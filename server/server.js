3d362afa2db82691d1bc431b43886cf9ab6d1dd0
Oriol Burgaya <oriolbv@gmail.com>
2020-06-27 13:08:26 +0200

    Listener and MongoDB connection added

--- server/server.js
+++ server/server.js
@@ -1,5 +1,32 @@
 import express from 'express'
 import devBundle from './devBundle'
+import path from 'path'
+import template from './../template'
+import { MongoClient } from 'mongodb'
+
+const CURRENT_WORKING_DIR = process.cwd()
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSimpleSetup'
 
 const app = express()
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 devBundle.compile(app)
+
+
+
+app.get('/', (req, res) => {
+     res.status(200).send(template())
+})
+
+let port = process.env.PORT || 3000
+app.listen(port, function onStart(err) {
+  if (err) {
+    console.log(err) 
+  }
+  console.info('Server started on port %s.', port)
+})
+
+
+MongoClient.connect(url, (err, db)=>{
+  console.log("Connected successfully to mongodb server")
+  db.close()
+})
