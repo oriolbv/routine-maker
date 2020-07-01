42a30ebad4cf08bd94706dfa918b0dc31c248d4c
Oriol Burgaya <oriolbv@gmail.com>
2020-07-01 22:54:41 +0200

    User controller added

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,12 @@
+import User from '../models/user.model'
+import _ from 'lodash'
+import errorHandler from './error.controller'
+
+const create = (req, res, next) => {  }
+const list = (req, res) => {  }
+const userByID = (req, res, next, id) => {  }
+const read = (req, res) => {  }
+const update = (req, res, next) => {  }
+const remove = (req, res, next) => {  }
+
+export default { create, userByID, read, list, remove, update }
