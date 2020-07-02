7698dbe62a9eb526feb637f48e2c3f5b7c40d59a
Oriol Burgaya <oriolbv@gmail.com>
2020-07-02 22:22:05 +0200

    Authentication methods implemented

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -3,9 +3,9 @@ import authCtrl from '../controllers/auth.controller'
 
 const router = express.Router()
 
-router.route('/auth/signin')
-    .post(authCtrl.signin)
-router.route('/auth/signout')
-    .get(authCtrl.signout)
+router.route('/auth/signin').post(authCtrl.signin)
+router.route('/auth/signout').get(authCtrl.signout)
+
+
 
 export default router
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,6 +1,6 @@
-
 import express from 'express'
 import userCtrl from '../controllers/user.controller'
+import authCtrl from '../controllers/auth.controller'
 
 const router = express.Router()
 
