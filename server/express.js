4920942a07007e38b34c9913cbb3c3496a59dec2
Oriol Burgaya <oriolbv@gmail.com>
2020-07-04 18:09:46 +0200

    More client dependencies and fonts added into template.js

--- server/express.js
+++ server/express.js
@@ -4,12 +4,18 @@ import cookieParser from 'cookie-parser'
 import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
+import path from 'path'
 
 import Template from './../template'
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
+import devBundle from './devBundle'
+
+const CURRENT_WORKING_DIR = process.cwd()
 
 const app = express()
+
+devBundle.compile(app)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -18,6 +24,8 @@ app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
 app.use(cors())
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 
 app.get('/', (req, res) => {
     res.status(200).send(Template())
--- template.js
+++ template.js
@@ -2,12 +2,14 @@ export default () => {
   return `<!doctype html>
     <html lang="en">
         <head>
-           <meta charset="utf-8">
-           <title>MERN Skeleton</title>
+            <meta charset="utf-8">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+            <title>MERN Skeleton</title>
         </head>
         <body>
-          <div id="root">User CRUD</div>
-          <script type="text/javascript" src="/dist/bundle.js"></script>
+            <div id="root">User CRUD</div>
+            <script type="text/javascript" src="/dist/bundle.js"></script>
         </body>
     </html>`
 }
