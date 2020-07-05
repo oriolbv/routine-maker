66cc2e1824d5a08072f84e7dc5b3115080753a92
Oriol Burgaya <oriolbv@gmail.com>
2020-07-05 10:34:19 +0200

    Hot reload dependencies

--- client/App.js
+++ client/App.js
@@ -4,6 +4,8 @@ import React from 'react'
 import MainRouter from './MainRouter'
 import {BrowserRouter} from 'react-router-dom'
 
+import { hot } from 'react-hot-loader'
+
 const theme = createMuiTheme({
     palette: {
         primary: {
@@ -31,3 +33,5 @@ const App = () => (
     </MuiThemeProvider>
   </BrowserRouter>
 )
+
+export default hot(module)(App)
