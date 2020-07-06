7c610184e8a3b88c66190ff40faf0ce348ce3d87
Oriol Burgaya <oriolbv@gmail.com>
2020-07-06 23:13:29 +0200

    Users list added into Users component

--- client/user/Users.js
+++ client/user/Users.js
@@ -3,4 +3,13 @@
 class Users extends Component {
     state = { users: [] }
 
+    componentDidMount = () => {
+        list().then((data) => {
+            if (data.error)
+                console.log(data.error)
+            else
+                this.setState({users: data})
+        })
+    }
+
 }
