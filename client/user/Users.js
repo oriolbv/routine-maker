120533724224b4e47eeab55736c44c22cce3bd96
Oriol Burgaya <oriolbv@gmail.com>
2020-07-07 19:49:53 +0200

    User view render added

--- client/user/Users.js
+++ client/user/Users.js
@@ -12,4 +12,34 @@ class Users extends Component {
         })
     }
 
+    render() {
+        const {classes} = this.props
+        return (
+            <Paper className={classes.root} elevation={4}>
+                <Typography type="title" className={classes.title}>
+                All Users
+                </Typography>
+                <List dense>
+                    {this.state.users.map(function(item, i) {
+                        return <Link to={"/user/" + item._id} key={i}>
+                            <ListItem button="button">
+                            <ListItemAvatar>
+                                <Avatar>
+                                <Person/>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary={item.name}/>
+                            <ListItemSecondaryAction>
+                                <IconButton>
+                                <ArrowForward/>
+                                </IconButton>
+                            </ListItemSecondaryAction>
+                            </ListItem>
+                        </Link>
+                    })}
+                </List>
+            </Paper>
+        )
+      }
+
 }
