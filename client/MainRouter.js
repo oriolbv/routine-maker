6d3e57f62942b77c198436d68135b61d3024db46
Oriol Burgaya <oriolbv@gmail.com>
2020-07-08 23:05:46 +0200

    Signup view added

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Home from './core/Home'
 import Users from './user/Users'
+import Signup from './user/Signup'
 
 class MainRouter extends Component {
   render() {
     return (<div>
       <Switch>
         <Route exact path="/" component={Home}/>
-        <Route exact path="/users" component={Users}/>
+        <Route path="/users" component={Users}/>
+        <Route path="/signup" component={Signup}/>
         {/* <Route exact path="/users" component={props => <Users {...props} />} /> */}
       </Switch>
     </div>)
