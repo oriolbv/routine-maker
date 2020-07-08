7f21765fcc806c52373a6b4f7038053bae0b271c
Oriol Burgaya <oriolbv@gmail.com>
2020-07-08 22:27:18 +0200

    Users component fixes

--- client/user/Users.js
+++ client/user/Users.js
@@ -1,49 +1,28 @@
 import React, {Component} from 'react'
-import Typography from 'material-ui/Typography'
+// import Typography from 'material-ui/Typography'
 import Paper from 'material-ui/Paper'
-import List from 'material-ui/List'
-import {Link} from 'react-router-dom'
+// import List from 'material-ui/List'
+// import {Link} from 'react-router-dom'
 
-class Users extends Component {
-    state = { users: [] }
+class Users extends React.Component {
+//     state = { users: [] }
 
-    componentDidMount = () => {
-        list().then((data) => {
-            if (data.error)
-                console.log(data.error)
-            else
-                this.setState({users: data})
-        })
-    }
+//     componentDidMount = () => {
+//         list().then((data) => {
+//             if (data.error)
+//                 console.log(data.error)
+//             else
+//                 this.setState({users: data})
+//         })
+//     }
 
     render() {
-        const {classes} = this.props
+//         const {classes} = this.props
         return (
             <Paper className={classes.root} elevation={4}>
-                <Typography type="title" className={classes.title}>
-                All Users
-                </Typography>
-                <List dense>
-                    {this.state.users.map(function(item, i) {
-                        return <Link to={"/user/" + item._id} key={i}>
-                            <ListItem button="button">
-                            <ListItemAvatar>
-                                <Avatar>
-                                <Person/>
-                                </Avatar>
-                            </ListItemAvatar>
-                            <ListItemText primary={item.name}/>
-                            <ListItemSecondaryAction>
-                                <IconButton>
-                                <ArrowForward/>
-                                </IconButton>
-                            </ListItemSecondaryAction>
-                            </ListItem>
-                        </Link>
-                    })}
-                </List>
+
             </Paper>
         )
-      }
+    }
 
 }
