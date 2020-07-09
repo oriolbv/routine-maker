c6961e2cc2ab048aef9edf22ef4dfb0b967172f5
Oriol Burgaya <oriolbv@gmail.com>
2020-07-09 21:58:16 +0200

    Signup indented correctly

--- client/user/Signup.js
+++ client/user/Signup.js
@@ -64,44 +64,44 @@ class Signup extends Component {
     })
   }
 
-  render() {
-    const {classes} = this.props
-    return (<div>
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography type="headline" component="h2" className={classes.title}>
-            Sign Up
-          </Typography>
-          <TextField id="name" label="Name" className={classes.textField} value={this.state.name} onChange={this.handleChange('name')} margin="normal"/><br/>
-          <TextField id="email" type="email" label="Email" className={classes.textField} value={this.state.email} onChange={this.handleChange('email')} margin="normal"/><br/>
-          <TextField id="password" type="password" label="Password" className={classes.textField} value={this.state.password} onChange={this.handleChange('password')} margin="normal"/>
-          <br/> {
-            this.state.error && (<Typography component="p" color="error">
-              <Icon color="error" className={classes.error}>error</Icon>
-              {this.state.error}</Typography>)
-          }
-        </CardContent>
-        <CardActions>
-          <Button color="primary" variant="raised" onClick={this.clickSubmit} className={classes.submit}>Submit</Button>
-        </CardActions>
-      </Card>
-      <Dialog open={this.state.open} disableBackdropClick={true}>
-        <DialogTitle>New Account</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            New account successfully created.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Link to="/signin">
-            <Button color="primary" autoFocus="autoFocus" variant="raised">
-              Sign In
-            </Button>
-          </Link>
-        </DialogActions>
-      </Dialog>
-    </div>)
-  }
+    render() {
+        const {classes} = this.props
+        return (<div>
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography type="headline" component="h2" className={classes.title}>
+                Sign Up
+              </Typography>
+              <TextField id="name" label="Name" className={classes.textField} value={this.state.name} onChange={this.handleChange('name')} margin="normal"/><br/>
+              <TextField id="email" type="email" label="Email" className={classes.textField} value={this.state.email} onChange={this.handleChange('email')} margin="normal"/><br/>
+              <TextField id="password" type="password" label="Password" className={classes.textField} value={this.state.password} onChange={this.handleChange('password')} margin="normal"/>
+              <br/> {
+                this.state.error && (<Typography component="p" color="error">
+                  <Icon color="error" className={classes.error}>error</Icon>
+                  {this.state.error}</Typography>)
+              }
+            </CardContent>
+            <CardActions>
+              <Button color="primary" variant="raised" onClick={this.clickSubmit} className={classes.submit}>Submit</Button>
+            </CardActions>
+          </Card>
+          <Dialog open={this.state.open} disableBackdropClick={true}>
+            <DialogTitle>New Account</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                New account successfully created.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Link to="/signin">
+                <Button color="primary" autoFocus="autoFocus" variant="raised">
+                  Sign In
+                </Button>
+              </Link>
+            </DialogActions>
+          </Dialog>
+        </div>)
+    }
 }
 
 Signup.propTypes = {
