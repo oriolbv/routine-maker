0330757cca4654f60e6954f67b4d0393ba8cf953
Oriol Burgaya <oriolbv@gmail.com>
2020-07-14 22:58:52 +0200

    Routines list page bugs fixed

--- client/routines/Routines.js
+++ client/routines/Routines.js
@@ -9,7 +9,7 @@ import Typography from '@material-ui/core/Typography'
 import ArrowForward from '@material-ui/icons/ArrowForward'
 import Person from '@material-ui/icons/Person'
 import {Link} from 'react-router-dom'
-import {list} from './api-user.js'
+// import {list} from './api-user.js'
 
 const styles = theme => ({
     root: theme.mixins.gutters({
@@ -27,50 +27,22 @@ class Routines extends Component {
         routines: []
     }
 
-    componentDidMount() {
-        list().then((data) => {
-            if (data.error) {
-                console.log(data.error)
-            } else {
-                this.setState({routines: data})
-            }
-        })
-    }
-
   render() {
         const {classes} = this.props
         return (
         <Paper className={classes.root} elevation={4}>
             <Typography type="title" className={classes.title}>
-            All Users
+            All Routines
             </Typography>
-            <List dense>
-            {this.state.users.map((item, i) => {
-            return <Link to={"/routine/" + item._id} key={i}>
-                        <ListItem button>
-                        <ListItemAvatar>
-                            <Avatar>
-                            <Person/>
-                            </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText primary={item.name}/>
-                        <ListItemSecondaryAction>
-                        <IconButton>
-                            <ArrowForward/>
-                        </IconButton>
-                        </ListItemSecondaryAction>
-                        </ListItem>
-                    </Link>
-                })
-                }
-            </List>
+                
+        
         </Paper>
         )
     }
 }
 
-Users.propTypes = {
-  classes: PropTypes.object.isRequired
-}
+// Users.propTypes = {
+//   classes: PropTypes.object.isRequired
+// }
 
-export default withStyles(styles)(Users)
+// export default withStyles(styles)(Users)
