919f44673b5a68e37b8b6c32f2534db7b92b6d5d
Oriol Burgaya <oriolbv@gmail.com>
2020-08-13 20:34:55 +0200

    Sport image added into home screen

--- client/core/Home.js
+++ client/core/Home.js
@@ -4,7 +4,7 @@ import Card from '@material-ui/core/Card'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
-import unicornbikeImg from './../assets/images/unicornbike.jpg'
+import SportImg from './../assets/images/sport.jpg'
 import Grid from '@material-ui/core/Grid'
 import auth from './../auth/auth-helper'
 import FindPeople from './../user/FindPeople'
@@ -62,8 +62,7 @@ export default function Home({history}){
                 <Typography variant="h6" className={classes.title}>
                   Home Page
                 </Typography>
-                <CardMedia className={classes.media} image={unicornbikeImg} title="Unicorn Bicycle"/>
-                <Typography variant="body2" component="p" className={classes.credit} color="textSecondary">Photo by <a href="https://unsplash.com/@boudewijn_huysmans" target="_blank" rel="noopener noreferrer">Boudewijn Huysmans</a> on Unsplash</Typography>
+                <CardMedia className={classes.media} image={SportImg} title="Routine Maker"/>
                 <CardContent>
                   <Typography type="body1" component="p">
                     Welcome to the Routine maker home page. 
