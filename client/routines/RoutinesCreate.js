31f3a9b0120b0466ecbdfa6e85c306f7d6f59c7a
Oriol Burgaya <oriolbv@gmail.com>
2020-09-15 22:15:49 +0200

    Routines list view working

--- client/routines/RoutinesCreate.js
+++ client/routines/RoutinesCreate.js
@@ -1,5 +1,19 @@
 import React, {useState, useEffect} from 'react'
 import {makeStyles} from '@material-ui/core/styles'
+import auth from './../auth/auth-helper'
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardContent from '@material-ui/core/CardContent'
+import CardActions from '@material-ui/core/CardActions'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
+import Avatar from '@material-ui/core/Avatar'
+import Icon from '@material-ui/core/Icon'
+import PropTypes from 'prop-types'
+import {create} from './api-routines.js'
+import IconButton from '@material-ui/core/IconButton'
+import PhotoCamera from '@material-ui/icons/PhotoCamera'
 
 
 
